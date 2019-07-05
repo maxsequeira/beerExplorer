7f27dade37f0932faaa9f7dc41b45984f784f2a9
Maximiliano Sequeira <maximiliano.sequeira@jointecnologia.com.br>
2019-07-05 15:15:12 -0300

    Clear data

--- BeerExplorer/src/main/java/com/senacrs/BeerExplorer/data/ApiData.java
+++
@@ -1,8 +0,0 @@
-package com.senacrs.BeerExplorer.data;
-
-public class ApiData {
-
-    public static final String USER_ID = "USER_ID";
-    public static final String USER_SECRET = "USER_SECRET";
-
-}
--- BeerExplorer/src/main/java/com/senacrs/BeerExplorer/data/ClientData.java
+++
@@ -1,9 +0,0 @@
-package com.senacrs.BeerExplorer.data;
-
-public class ClientData {
-
-    public static final String USERNAME = "usar";
-    public static final String PASS = "";
-    public static final String LOCAL= "";
-
-}
